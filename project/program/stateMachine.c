0661a91d84513b571eb8cd105447bf36dc7af220
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2024-04-20 17:20:47 -0600

    Update stateMachine.c
    Added switch-case statement for state machine

--- project/program/stateMachine.c
+++ project/program/stateMachine.c
@@ -8,29 +8,32 @@ void stateMachine()
 {
   char p1val = P1IN;  //define botton from bottom
   char p2val = P2IN;  //help detects upper bottons
- 
-  if( p2val & TOPS1 ? 0 : 1 )
-    { //bottom 1 a friendly melody :D
+  
+  switch (p2val) {
+    case TOPS1:
+      //bottom 1: a friendly melody
       P1OUT &= ~LED_RED;
       melody(0); 
-    }
-  else if( p2val & TOPS2 ? 0 : 1)
-    { //bottom 2 random sound and dimmy lights
+      break;
+      
+    case TOPS2:
+      //bottom 2: random sound and dim lights
       randomSound();
-    }
-  else if( p2val & TOPS3 ? 0 : 1)
-    {
-      // A sound that corresponds with the lights.
+      break;
+      
+    case TOPS3:
+      //bottom 3: a sound that corresponds with the lights
       alarmSound();
-    }
-  else if( p2val & TOPS4 ? 0 : 1){
-    
-  }
-  else if (p1val & SW1 ? 0 : 1){
-   
-  }
-  else{
-    buzzer_set_period(0);
-    P1OUT &= ~LEDS;
+      break;
+      
+    case TOPS4:
+      //bottom 4: no action
+      break;
+      
+    default:
+      //no button pressed: turn off buzzer and LEDs
+      buzzer_set_period(0);
+      P1OUT &= ~LEDS;
+      break;
   }
 }

e8d6ffabfbd98a2e5b86ef49787c503dd76a40ae
erquezada <erquezada@miners.utep.edu>
2024-04-22 13:04:34 -0600

    Fixed code. Everything works.

--- project/program/stateMachine.c
+++ project/program/stateMachine.c
@@ -1,39 +1,37 @@
-
 #include <msp430.h>
 #include "libTimer.h"
 #include "buzzer.h"
 #include "led.h"
 
+void randomSound();
+void alarmSound();
 void stateMachine()
 {
   char p1val = P1IN;  //define botton from bottom
   char p2val = P2IN;  //help detects upper bottons
-  
-  switch (p2val) {
-    case TOPS1:
-      //bottom 1: a friendly melody
+
+  if( p2val & TOPS1 ? 0 : 1 )
+    { //bottom 1 a friendly melody :D
       P1OUT &= ~LED_RED;
-      melody(0); 
-      break;
-      
-    case TOPS2:
-      //bottom 2: random sound and dim lights
+      melody(0);
+    }
+  else if( p2val & TOPS2 ? 0 : 1)
+    { //bottom 2 random sound and dimmy lights
       randomSound();
-      break;
-      
-    case TOPS3:
-      //bottom 3: a sound that corresponds with the lights
+    }
+  else if( p2val & TOPS3 ? 0 : 1)
+    {
+      // A sound that corresponds with the lights.
       alarmSound();
-      break;
-      
-    case TOPS4:
-      //bottom 4: no action
-      break;
-      
-    default:
-      //no button pressed: turn off buzzer and LEDs
-      buzzer_set_period(0);
-      P1OUT &= ~LEDS;
-      break;
+    }
+  else if( p2val & TOPS4 ? 0 : 1){
+
+  }
+  else if (p1val & SW1 ? 0 : 1){
+
+  }
+  else{
+    buzzer_set_period(0);
+    P1OUT &= ~LEDS;
   }
 }

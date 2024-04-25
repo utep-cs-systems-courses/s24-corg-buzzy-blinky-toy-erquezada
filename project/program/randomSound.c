c5a0f16030503cbb4d4ce388e1ec747a37fef737
erquezada <erquezada@miners.utep.edu>
2024-04-24 19:17:11 -0600

    Final Commit

--- project/program/randomSound.c
+++ project/program/randomSound.c
@@ -3,12 +3,14 @@
 #include "buzzer.h"
 #include "led.h"
 
-// Function to play the melody
-void randomSound() {
-  float notes = {587.33, 523.25, 659.25, 587.33, 698.46, 659.25, 698.46};
+float notes[] = {587.33, 523.25, 659.25, 587.33, 698.46, 659.25, 698.46};
 // Notes are F5, E5, F5, D5, E5, C5, D5
 // Function to control the LED dimming from brightest to dimmest
 
+ 
+
+// Function to play the melody
+void randomSound() {
   int numNotes = sizeof(notes) / sizeof(notes[0]);
   // Repeat the melody and LED dimming effect four times
   for (int repeat = 0; repeat < 4; repeat++) {
